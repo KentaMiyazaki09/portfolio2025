db3254185acf5295d45d3829d1f57e213e547a76
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2025-08-28 17:31:18 +0900

    タイトル周りの調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { motion } from "motion/react";
 
 import Styles from "@/styles/page.module.css";
@@ -15,13 +15,13 @@ const TopPage = () => {
         exit={{ opacity: 0, y: 10 }}
         transition={{ duration: 0.5 }}
       >
-        <h1 className="text-[clamp(1.125rem,1.875vw,1.5rem)] font-semibold tracking-widest [text-shadow:2px_2px_0_black] opacity-80 mb-3">
+        <h1 className="text-[clamp(1.5rem,1.875vw,2.0rem)] font-semibold tracking-widest [text-shadow:2px_2px_0_black] mb-3">
           KENTA MIYAZAKI
         </h1>
         <p className="text-[clamp(1.875rem,3.75vw,3rem)] leading-[1.2] font-extrabold [text-shadow:2px_2px_0_black]">
           <span className={Styles.hasBorder}>Front Developer</span>
           <br />
-          3D <span className="opacity-80">&</span> Web
+          3D <span>&</span> Web
         </p>
       </motion.div>
     </div>
--- src/components/atoms/PageTtl.tsx
+++ src/components/atoms/PageTtl.tsx
@@ -1,10 +1,19 @@
+import { motion } from "motion/react";
+
 type Props = {
   text: string;
 };
 
 const PageTtl = ({ text }: Props) => (
-  <h1 className="z-30 fixed top-[40%] left-[-10px] rotate-90 text-[clamp(1.1rem,2.5vw,2rem)] font-semibold">
-    {text}
+  <h1 className="z-30 fixed top-[20%] left-[-10px] rotate-90 text-[clamp(1.1rem,2.5vw,2rem)] font-semibold">
+    <motion.div
+      initial={{ opacity: 0, x: 10 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 10 }}
+      transition={{ duration: 0.5 }}
+    >
+      {text}
+    </motion.div>
   </h1>
 );
 
