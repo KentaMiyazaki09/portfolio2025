87f9424dc8c23a3af9adb6ac83aa4d1b61083cd2
KentaMiyazaki09 <kmiyazaki0924@gmail.com>
2025-09-06 01:03:13 +0900

    ページごとに色が変わるように設定

--- src/components/atoms/BgWiper.tsx
+++ src/components/atoms/BgWiper.tsx
@@ -6,22 +6,30 @@ import { useEffect, useRef, useState } from "react";
 const TextParticles = () => {
   const pathname = usePathname();
   const [mount, setMount] = useState(false);
+
   const [show, setShow] = useState(false);
-  const [color, setColor] = useState("#fff");
+  const [showWorks, setShowWorks] = useState(false);
+
   const ref = useRef<SVGRectElement | null>(null);
+  const refWorks = useRef<SVGRectElement | null>(null);
 
   useEffect(() => setMount(true), []);
 
   // ページ遷移時に表示ON
   useEffect(() => {
     if (pathname === "/about") {
-      setColor("#4169e1");
       setShow(true);
+      setShowWorks(false);
+    } else if (pathname === "/works") {
+      setShowWorks(true);
+      setShow(false);
     } else {
+      setShowWorks(false);
       setShow(false);
     }
   }, [pathname]);
 
+  /* aboutの背景 */
   useEffect(() => {
     const el = ref.current;
     if (!el) return;
@@ -53,7 +61,40 @@ const TextParticles = () => {
     }
   }, [show]);
 
+  /* worksの背景 */
+  useEffect(() => {
+    const el = refWorks.current;
+    if (!el) return;
+
+    if (showWorks) {
+      el.animate(
+        [
+          { transform: "translateY(120%)", opacity: 1 },
+          { transform: "translateY(0%)", opacity: 1 },
+        ],
+        {
+          duration: 500,
+          easing: "linear",
+          fill: "forwards",
+        }
+      );
+    } else {
+      el.animate(
+        [
+          { transform: "translateY(0%)", opacity: 1 },
+          { transform: "translateY(-120%)", opacity: 1 },
+        ],
+        {
+          duration: 500,
+          easing: "linear",
+          fill: "forwards",
+        }
+      );
+    }
+  }, [showWorks]);
+
   if (!mount) return;
+
   return (
     <div className="fixed inset-0 z-0 pointer-events-none" aria-hidden>
       <svg
@@ -61,6 +102,7 @@ const TextParticles = () => {
         height="100%"
         viewBox="0 0 100 100"
         preserveAspectRatio="none"
+        className="absolute"
       >
         <defs>
           <mask id="wipe-mask">
@@ -78,7 +120,43 @@ const TextParticles = () => {
         </defs>
 
         {/* フィルレイヤー */}
-        <rect width="100%" height="100%" fill={color} mask="url(#wipe-mask)" />
+        <rect
+          width="100%"
+          height="100%"
+          fill="#283655"
+          mask="url(#wipe-mask)"
+        />
+      </svg>
+
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+        className="absolute"
+      >
+        <defs>
+          <mask id="wipe-mask-works">
+            <rect width="100%" height="100%" fill="black" />
+            <rect
+              ref={refWorks}
+              x="0"
+              y="0"
+              width="100%"
+              height="120%"
+              fill="white"
+              transform="translate(0, 120)"
+            />
+          </mask>
+        </defs>
+
+        {/* フィルレイヤー */}
+        <rect
+          width="100%"
+          height="100%"
+          fill="#4d648d"
+          mask="url(#wipe-mask-works)"
+        />
       </svg>
     </div>
   );
