4148376e68013450b010b5a4e7c2ef8791eceeee
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2025-09-22 18:45:03 +0900

    - worksにコメントを追加

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -8,51 +8,62 @@ import Works from "@/components/molecules/Works";
 const list = [
   {
     thumb: "/works/Portfolio.png",
-    url: "https://nowledge.rhino-inc.jp/",
+    url: "https://portfolio2025-delta-seven.vercel.app/",
     ttl: "Portfolio",
     tags: ["Next.js", "Blender"],
+    comment: "BlenderやWEBサイトの作品ギャラリー、または新しい技術の実験場。",
   },
   {
     thumb: "/works/nowledge.png",
-    url: "https://portfolio2025-delta-seven.vercel.app/",
+    url: "https://nowledge.rhino-inc.jp/",
     ttl: "Nowledge",
     tags: ["Next.js", "jamstack"],
+    comment: "社内で週1開催される情報共有をスプシからWEBサイトへ。",
   },
   {
     thumb: "/works/neutrial.png",
     url: "https://neutrial.com/",
     ttl: "neutrial",
-    tags: ["Shopify"],
+    tags: ["Shopify", "liquid"],
+    comment: "liquidを用いて部分的に更新を担当。ともさかりえさんのサプリ。",
   },
   {
     thumb: "/works/Beams.png",
     url: "https://www.beams.co.jp/special/beams_cultuart/",
     ttl: "Beams Cultuart",
     tags: ["Wordpress"],
+    comment: "Beamsの新規サイト。様々なカルチャーにまつわる記事を発信中⚡️。",
   },
   {
     thumb: "/works/nikoand.png",
     url: "https://www.dot-st.com/nikoand/cp/202408product_zakka",
     ttl: "nikoand... キン肉マンコラボ",
     tags: ["LP", "Canvas"],
+    comment: "レトロ感あるモザイクアニメーションをCanvasで実装しました。",
   },
   {
     thumb: "/works/THREE.png",
     url: "https://www.threecosmetics.com/brand/news/fragrance-finder",
     ttl: "THREE FRAGRANCE FINDER",
     tags: ["LP", "Canvas"],
+    comment:
+      "javascriptのclassでゴリゴリ状態管理🦍。背景のCanvasはGradient.jsというライブラリー。",
   },
   {
     thumb: "/works/tku.png",
     url: "https://www.tku.ac.jp/",
     ttl: "東京経済大学",
     tags: ["Movabletype7"],
+    comment:
+      "Movabletype7を勉強しながら実装したサイト。ちゃんと更新されてて満足🍔",
   },
   {
     thumb: "/works/kidsattic.png",
     url: "https://www.kids-attic.org/",
     ttl: "キッズアティック",
     tags: ["LP"],
+    comment:
+      "懐かしのサイト。ローディングロゴはデザイナーさんに1パーツずつ分けてもらった🙏",
   },
 ];
 
--- src/components/molecules/Works.tsx
+++ src/components/molecules/Works.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  CardTitle,
+} from "@/components/ui/card";
 
 type ListItem = {
   thumb: string;
   url: string;
   ttl: string;
   tags: string[];
+  comment: string;
 };
 
 type ListProps = {
@@ -16,8 +23,8 @@ type ListProps = {
 const Works = ({ list }: ListProps) => {
   return (
     <div className="md:flex md:flex-wrap md:justify-between">
-      {list.map(({ ttl, url, thumb, tags }) => (
-        <Card key={ttl} className="bg-black/20 w-fill md:w-[49%] mb-3">
+      {list.map(({ ttl, url, thumb, tags, comment }) => (
+        <Card key={ttl} className="bg-black/70 w-fill md:w-[49%] mb-3">
           <a href={url} target="_blank" rel="noopener noreferrer">
             <CardHeader className="pb-4">
               <div className="relative w-full h-[100px] md:h-[150px] mb-3">
@@ -31,6 +38,9 @@ const Works = ({ list }: ListProps) => {
               </div>
               <CardTitle className="text-neutral-100 text-xl">{ttl}</CardTitle>
             </CardHeader>
+            <CardContent>
+              <p className="text-sm text-neutral-200">{comment}</p>
+            </CardContent>
             <CardFooter>
               {tags.map((tag) => (
                 <span key={tag} className="text-neutral-300 text-sm mr-2">
