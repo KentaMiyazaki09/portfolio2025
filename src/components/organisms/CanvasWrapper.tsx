66970744ad16b125821a0509fe9bc3d9378cfac3
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2025-11-17 10:30:24 +0900

    - buildエラー - コメントをReactの形式に修正

--- src/components/organisms/CanvasWrapper.tsx
+++ src/components/organisms/CanvasWrapper.tsx
@@ -20,30 +20,15 @@ const CanvasWrapper: React.FC = () => {
 
   return (
     <div className={`${styles.wrapper} z-10`}>
-      <Canvas
-        shadows
-        className="touch-none z-[100]"
-      >
-        {/* <Environment files="/hdri/night_puresky.hdr" background={false} environmentIntensity={0.3} /> */}
+      <Canvas shadows className="touch-none z-[100]">
+        {/** 前方から照らすライト */}
+        <directionalLight intensity={2} position={[0, 0, 5]} castShadow />
 
-        /** 前方から照らすライト */
-        <directionalLight
-          intensity={2}
-          position={[0, 0, 5]}
-          castShadow
-        />
+        {/** 上から照らすライト */}
+        <directionalLight intensity={0.8} position={[0, 10, 8]} />
 
-        /** 上から照らすライト */
-        <directionalLight
-          intensity={0.8}
-          position={[0, 10, 8]}
-        />
-
-        /** 逆光（後ろから照らすライト） */
-        <directionalLight
-          intensity={0.8}
-          position={[-2, 1, -3]}
-        />
+        {/** 逆光（後ろから照らすライト） */}
+        <directionalLight intensity={0.8} position={[-2, 1, -3]} />
 
         <PerspectiveCamera
           makeDefault
