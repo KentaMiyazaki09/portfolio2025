5d0a7a7f2617a96c004c7a712f100c3fe4d94c28
KentaMiyazaki09 <kmiyazaki0924@gmail.com>
2026-02-14 19:33:01 +0900

    meta情報の追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,11 +16,34 @@ import BgWiper from "@/components/organisms/BgWiper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://portfolio2025-delta-seven.vercel.app/"),
   title: "K.Miyazaki portfolio",
   description:
-    "Kenta Miyazaki ポートフォリオ。1995年生まれ。フロントエンジニア。アイデアを視覚や感覚で伝える表現に落とし込むのが好きです。WebGL・Blenderを勉強中。",
+    "Kenta Miyazaki ポートフォリオ。1995年生まれ。フロントエンジニア。視覚や感覚的な表現に落とし込むのが好きです。React、WebGL・Blenderを勉強中。",
   icons: {
     icon: "/favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    url: "/",
+    title: "K.Miyazaki portfolio",
+    description:
+      "Kenta Miyazaki ポートフォリオ。1995年生まれ。フロントエンジニア。視覚や感覚的な表現に落とし込むのが好きです。React、WebGL・Blenderを勉強中。",
+    images: [
+      {
+        url: "/ogp.jpg",
+        width: 1200,
+        height: 630,
+        alt: "K.Miyazaki portfolio OGP",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "K.Miyazaki portfolio",
+    description:
+      "Kenta Miyazaki ポートフォリオ。1995年生まれ。フロントエンジニア。視覚や感覚的な表現に落とし込むのが好きです。React、WebGL・Blenderを勉強中。",
+    images: ["/ogp.jpg"],
   },
   other: {
     "vercel-toolbar": "disable",
